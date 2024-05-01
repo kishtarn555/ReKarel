20638b4b2dcc2f86eaa1c9056adf975970f7355e
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2024-05-01 14:51:14 -0600

    Better Track Karel

--- webapp/js/cindex.js
+++ webapp/js/cindex.js
@@ -22522,13 +22522,13 @@
                 tr = this.world.i;
             }
             else if (this.world.i >= tr + rows) {
-                tr = this.world.i - rows;
+                tr = this.world.i - rows - 1;
             }
             if (this.world.j < tc) {
                 tc = this.world.j;
             }
             else if (this.world.j >= tc + cols) {
-                tc = this.world.j - cols;
+                tc = this.world.j - cols - 1;
             }
             console.log("New Focus @" + `${tr} ,${tc} `);
             this.FocusTo(tr, tc);
