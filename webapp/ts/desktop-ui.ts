84a1963337d777c1ba0832dd85b1f0ec5ff19d6b
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2024-05-03 17:16:56 -0600

    Removed deprecated toggle code

--- webapp/ts/desktop-ui.ts
+++ webapp/ts/desktop-ui.ts
@@ -564,29 +564,4 @@ class DesktopController {
 }
 
 
-//=================================== TO BE DELETED FROM HERE DOWNWARDS ===================================
-
-
-
-
-
-function toggleInfinityBeepers () {
-    if ($("#beeperBag").attr("hidden")!== undefined) { 
-        $("#beeperBag").removeAttr("hidden");
-        $("#beeperBag").val("0");
-        $("#infiniteBeepersBtn").removeClass("btn-info");
-        
-        $("#infiniteBeepersBtn").addClass("btn-light");            
-    } else {
-        $("#beeperBag").attr("hidden", "");
-        $("#beeperBag").val("-1");
-        $("#infiniteBeepersBtn").removeClass("btn-light");            
-        $("#infiniteBeepersBtn").addClass("btn-info");
-    }
-}
-
-
-
-
-
 export {DesktopController};
