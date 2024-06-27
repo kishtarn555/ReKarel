f3635dad43b8ddd169018787e22b9449e50e04ad
kishtarn555 <hec_montemoon@hotmail.com>
2024-06-27 15:26:09 -0600

    [UX] Change theme colors for more color consistency

--- webapp/js/cindex.js
+++ webapp/js/cindex.js
@@ -24442,7 +24442,7 @@ var karel = (function (exports, bootstrap) {
         disabled: '#4f4f4f',
         exportCellBackground: '#f5f7a8',
         karelColor: '#3E6AC1',
-        gridBackgroundColor: '#f8f9fA',
+        gridBackgroundColor: '#ffffff',
         errorGridBackgroundColor: "#f5d5d5",
         gridBorderColor: '#c4c4c4',
         errorGridBorderColor: '#a8838f',
@@ -24459,7 +24459,7 @@ var karel = (function (exports, bootstrap) {
         disabled: '#4f4f4f',
         exportCellBackground: '#f5f7a8',
         karelColor: '#3E6AC1',
-        gridBackgroundColor: '#f8f9fA',
+        gridBackgroundColor: '#ffffff',
         errorGridBackgroundColor: "#f5d5d5",
         gridBorderColor: '#c4c4c4',
         errorGridBorderColor: '#a8838f',
@@ -24493,7 +24493,7 @@ var karel = (function (exports, bootstrap) {
         disabled: '#4f4f4f',
         exportCellBackground: '#f5f7a8',
         karelColor: '#3F69DB',
-        gridBackgroundColor: '#f3f3f3',
+        gridBackgroundColor: '#ffffff',
         errorGridBackgroundColor: "#5D3D3D",
         gridBorderColor: '#565656',
         errorGridBorderColor: '#565656',
@@ -24658,7 +24658,7 @@ var karel = (function (exports, bootstrap) {
 
     const ReKarelHighlight = {
         color: "#fafafa",
-        backgroundColor: "var(--bs-dark)",
+        backgroundColor: "rgb(var(--bs-body-bg-rgb))",
         gutterBackgroundColor: "var(--bs-secondary-bg)",
         gutterColor: "var(--bs-emphasis-color)",
         extensions: [
@@ -24683,7 +24683,7 @@ var karel = (function (exports, bootstrap) {
     };
     const LightReKarelHighlight = {
         color: "#2e2e2e",
-        backgroundColor: "var(--bs-light)",
+        backgroundColor: "rgb(var(--bs-body-bg-rgb))",
         gutterBackgroundColor: "var(--bs-secondary-bg)",
         gutterColor: "var(--bs-emphasis-color)",
         extensions: [
