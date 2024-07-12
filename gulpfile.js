2f19928406976c186190abba146c61308f5d3c65
kishtarn555 <hec_montemoon@hotmail.com>
2024-07-12 11:39:32 -0600

    [gulp][dev][package] build scripts for transpilers

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,30 @@ gulp.task('process-jison-pascal', ()=> {
 
 })
 
+
+gulp.task('process-jison-java2pascal', ()=> {
+
+    const path = 'js/java2pascal.js'
+    return gulp.src(path) 
+        .pipe(insert.append('\nfunction java2pascalParser () {\n    return java2pascal.parse.apply(java2pascal, arguments);\n}\nexport {java2pascal, java2pascalParser}'))
+        .pipe(replace("_token_stack:", '// _token_stack:'))
+        .pipe(replace("loc: yyloc,", 'loc: lexer.yylloc, // Implement fix: https://github.com/zaach/jison/pull/356'))
+        .pipe(gulp.dest('js/')); 
+
+})
+
+
+gulp.task('process-jison-pascal2java', ()=> {
+
+    const path = 'js/pascal2java.js'
+    return gulp.src(path) 
+        .pipe(insert.append('\nfunction pascal2javaParser () {\n    return pascal2java.parse.apply(pascal2java, arguments);\n}\nexport {pascal2java, pascal2javaParser}'))
+        .pipe(replace("_token_stack:", '// _token_stack:'))
+        .pipe(replace("loc: yyloc,", 'loc: lexer.yylloc, // Implement fix: https://github.com/zaach/jison/pull/356'))
+        .pipe(gulp.dest('js/')); 
+
+})
+
 const paths = {
     html: [
         './webapp/index.html',
