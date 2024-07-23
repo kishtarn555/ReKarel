bf14d0c8f6aa6ea0f6a388519b7f74fa177fb4c5
kishtarn555 <hec_montemoon@hotmail.com>
2024-07-22 23:23:47 -0600

    [CodeMirror][Mobile] Remove other editor

--- webapp/ts/editor/editor.ts
+++ webapp/ts/editor/editor.ts
@@ -56,43 +56,12 @@ function createEditors() : Array<EditorView> {
       ])
     ]
   })
-  let otherState = EditorState.create({
-    doc: startState.doc,
-    extensions: [
-      drawSelection(),
-      lineNumbers(),
-      highlightActiveLine(),
-      keymap.of([
-        indentWithTab,
-        ...defaultKeymap,
-        {key: "Mod-z", run: () => undo(mainView)},
-        {key: "Mod-y", mac: "Mod-Shift-z", run: () => redo(mainView)}
-      ])
-    ]
-  })
-  let syncAnnotation = Annotation.define<boolean>()
-  
-  function syncDispatch(tr: Transaction, view: EditorView, other: EditorView) {
-    view.update([tr])
-    if (!tr.changes.empty && !tr.annotation(syncAnnotation)) {
-      let annotations: Annotation<any>[] = [syncAnnotation.of(true)]
-      let userEvent = tr.annotation(Transaction.userEvent)
-      if (userEvent) annotations.push(Transaction.userEvent.of(userEvent))
-        other.dispatch({changes: tr.changes, annotations})
-    }
-  }
   let mainView = new EditorView({
     state: startState,
     parent: document.querySelector("#splitter-left-top-pane"),
-    dispatch: tr => syncDispatch(tr, mainView, otherView)
   })
   
-  let otherView = new EditorView({
-    state: otherState,
-    parent: document.querySelector("#phoneEditor"),
-    dispatch: tr => syncDispatch(tr, otherView, mainView)
-  })
-  return [mainView, otherView]
+  return [mainView]
 }
 
 
