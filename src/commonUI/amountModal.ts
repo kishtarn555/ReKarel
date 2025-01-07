3edcfc238f55dc5d289c6cd4836b1cb7315af079
kishtarn555 <hec_montemoon@hotmail.com>
2025-01-06 20:55:31 -0600

    [UX] When pressing enter on the inputField, it accepts the value

--- src/commonUI/amountModal.ts
+++ src/commonUI/amountModal.ts
@@ -13,6 +13,11 @@ export function HookAmountModal(modal: AmountModal, worldController:WorldViewCon
     $(modal.modal).on("shown.bs.modal", () => {
         $(modal.inputField).trigger("focus");
     })
+    $(modal.inputField).on("keydown", (event) => {
+        if (event.key === "Enter") {
+            $(modal.confirmBtn).trigger("click");
+        }
+    })
     $(modal.confirmBtn).on("click", ()=> {
         const inputValue = $(modal.inputField).val();
         if (inputValue === "") {
