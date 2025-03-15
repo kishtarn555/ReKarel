410eadf87838028ff0c8624b510246b98adbba7b
kishtarn555 <hec_montemoon@hotmail.com>
2025-03-14 23:21:58 -0600

    [Cypress][Test] Test file save

---
+++ cypress/e2e/test_files.cy.ts
@@ -0,0 +1,41 @@
+import { World } from "@rekarel/core";
+import type {GetKarelController } from "../../src/index"
+describe('Test save idempotency', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('test_file_save', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:5500/webapp/');
+    cy.wait(500);
+    cy.get('body').type(
+        '{rightarrow}{rightarrow}{rightarrow}z{rightarrow}{rightarrow}z{rightarrow}'+
+        '{rightarrow}z{leftarrow}{uparrow}{uparrow}ee{leftarrow}{leftarrow}{leftarrow}2'+
+        '{uparrow}{leftarrow}{uparrow}{leftarrow}{uparrow}{leftarrow}e'+
+        '{rightarrow}{rightarrow}{rightarrow}{rightarrow}1'
+    );
+    cy.wait(500);
+    cy.get('#beeperBag').clear('04');
+    cy.get('#beeperBag').type('04');
+    cy.get('.container-md > .btn-toolbar').click();
+    cy.get('#evaluatorBtnDesktop > .d-none').click();
+    cy.wait(500);
+    cy.get('#evaluatePosition').check();
+    cy.get('#evaluateOrientation').check();
+    cy.get('#evaluateBag').check();
+    cy.get('#evaluateUniverse').check();
+    cy.get('#countMoves').check();
+    cy.get('#countTurns').check();
+    cy.get('#countPicks').check();
+    cy.get('#countPuts').check();
+    cy.get('#evaluatorForm > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+    cy.get('#navbarDropdownMenuLink').click();
+    cy.get('.navbar-nav > :nth-child(1) > .dropdown-menu > :nth-child(6) > .dropdown-item').click();
+
+    cy.get('#worldDataIn').should(
+      'have.value',
+      '<ejecucion version="1.1">\n\t<condiciones instruccionesMaximasAEjecutar="10000000" longitudStack="65000" memoriaStack="65000" llamadaMaxima="5"/>\n\t<mundos>\n\t\t<mundo nombre="mundo_0" ancho="100" alto="100">\n\t\t\t<monton x="4" y="3" zumbadores="2"/>\n\t\t\t<monton x="7" y="3" zumbadores="2"/>\n\t\t\t<monton x="1" y="6" zumbadores="1"/>\n\t\t\t<monton x="5" y="6" zumbadores="1"/>\n\t\t\t<posicionDump x="4" y="1"/>\n\t\t\t<posicionDump x="6" y="1"/>\n\t\t\t<posicionDump x="8" y="1"/>\n\t\t</mundo>\n\t</mundos>\n\t<programas tipoEjecucion="CONTINUA" intruccionesCambioContexto="1" milisegundosParaPasoAutomatico="0">\n\t\t<programa nombre="p1" ruta="{$2$}" mundoDeEjecucion="mundo_0" xKarel="1" yKarel="1" direccionKarel="NORTE" mochilaKarel="4">\n\t\t\t<despliega tipo="AVANZA"/>\n\t\t\t<despliega tipo="COGE_ZUMBADOR"/>\n\t\t\t<despliega tipo="DEJA_ZUMBADOR"/>\n\t\t\t<despliega tipo="GIRA_IZQUIERDA"/>\n\t\t\t<despliega tipo="MOCHILA"/>\n\t\t\t<despliega tipo="MUNDO"/>\n\t\t\t<despliega tipo="ORIENTACION"/>\n\t\t\t<despliega tipo="POSICION"/>\n\t\t\t<despliega tipo="UNIVERSO"/>\n\t\t</programa>\n\t</programas>\n</ejecucion>\n'
+    );
+
+    /* ==== End Cypress Studio ==== */
+  });
+});
+  
